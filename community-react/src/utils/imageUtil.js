42243d8d3634cd57c013457494fe72ceb4a816a4
kaa08 <kaa08@naver.com>
2025-12-01 17:33:53 +0900

    fix: image url updatE

--- community-react/src/utils/imageUtil.js
+++ community-react/src/utils/imageUtil.js
@@ -1,7 +1,7 @@
 export const S3_BASE_URL =
     "https://ktb-dori-bucket.s3.ap-northeast-2.amazonaws.com";
 
-export const DEFAULT_PROFILE_IMAGE = "/public/images/user.svg";
+export const DEFAULT_PROFILE_IMAGE = "/images/user.svg";
 
 export function buildImageUrl(key) {
     if (!key) return DEFAULT_PROFILE_IMAGE;
